b369898974f91f507a2e7ffce65759b330e02c85
Lisk <liskqwq@gmail.com>
2024-09-28 00:20:13 +0800

    ✨ feat: add hitokoto component

---
+++ docs/.vitepress/theme/components/Hitokoto.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+let hitokoto=ref("loading...")
+
+fetch("https://v1.hitokoto.cn").then(a=>a.json()).then((data)=>{
+  hitokoto.value=
+  `${data.hitokoto}
+  \n\t${data.from} - ${data.from_who}
+  `
+  })
+</script>
+<template>
+<p>{{hitokoto}}</p>
+</template>
+<style scoped>
+p {
+  color:#333;
+  margin-top:10px;
+  font-family:"rubik";
+  font-weight:lighter;
+}
+html.dark p{
+  color:#eee
+}
+</style>
